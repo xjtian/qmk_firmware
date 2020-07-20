d388a3fc97210478c40602cb42a7af5ee40d59aa
Jacky Tian <jacky@xjtian.com>
2020-07-20 03:35:14 -0700

    Add rotary encoder

--- keyboards/planck/keymaps/xjtian/config.h
+++ keyboards/planck/keymaps/xjtian/config.h
@@ -7,3 +7,9 @@
 #define PERMISSIVE_HOLD
 #define TAPPING_FORCE_HOLD 
 #define TAPPING_TERM 100
+
+#define ENCODERS_PAD_A { B12 }
+#define ENCODERS_PAD_B { B13 }
+
+#define ENCODER_RESOLUTION 1
+#define MEDIA_KEY_DELAY 10
