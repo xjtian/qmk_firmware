d0db83b9e562c3533da31ff8c8f247986d301130
Jacky Tian <jacky@xjtian.com>
2020-07-20 03:35:14 -0700

    Add rotary encoder

--- keyboards/planck/keymaps/xjtian/keymap.c
+++ keyboards/planck/keymaps/xjtian/keymap.c
@@ -26,14 +26,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-------+------+------+-------+------+---------+-------+----------+-------+--------+------+-----------|
  * | Shift |   Z  |   X  |   C   |   V  |    B    |   N   |     M    |   ,   |   .    |   /  |Enter/Shift|
  * |-------+------+------+-------+------+---------+-------+----------+-------+--------+------+-----------|
- * |  Ctrl |L(Fn) | Alt  |   OS  |L(Sym)|Shft/Bksp| Space |Ent/L(Num)|OS/Left|Alt/Down|Ctl/Up|   Right   |
+ * | Play  |L(Fn) | Alt  |   OS  |L(Sym)|Shft/Bksp| Space |Ent/L(Num)|OS/Left|Alt/Down|Ctl/Up|   Right   |
  * `-----------------------------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_4x12(
     KC_TAB,         KC_Q,  KC_W,    KC_E,     KC_R, KC_T,           KC_Y,   KC_U, KC_I,           KC_O,           KC_P,           KC_BSPC,
     CTL_T(KC_ESC),  KC_A,  KC_S,    KC_D,     KC_F, KC_G,           KC_H,   KC_J, KC_K,           KC_L,           KC_SCLN,        KC_QUOT,
     KC_LSFT,        KC_Z,  KC_X,    KC_C,     KC_V, KC_B,           KC_N,   KC_M, KC_COMM,        KC_DOT,         KC_SLSH,        RSFT_T(KC_ENT),
-    KC_LCTL,        MOF,   KC_LALT, KC_LGUI,  MOS,  SFT_T(KC_BSPC), KC_SPC, LTN,  GUI_T(KC_LEFT), ALT_T(KC_DOWN), CTL_T(KC_UP),   KC_RGHT
+    KC_MPLY,        MOF,   KC_LALT, KC_LGUI,  MOS,  SFT_T(KC_BSPC), KC_SPC, LTN,  GUI_T(KC_LEFT), ALT_T(KC_DOWN), CTL_T(KC_UP),   KC_RGHT
 ),
 
 
