461216877a6f28c61520220f85a08795b2b0cecd
Jacky Tian <jacky@xjtian.com>
2020-11-15 03:03:56 -0800

    add starcraft preonic layout

---
+++ keyboards/preonic/keymaps/xjtian/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef AUDIO_ENABLE
+    #define STARTUP_SONG SONG(PREONIC_SOUND)
+#endif
+
+#define PERMISSIVE_HOLD
+#define TAPPING_FORCE_HOLD 
+#define TAPPING_TERM 100
+
+#define ENCODER_RESOLUTION 1
+#define MEDIA_KEY_DELAY 10
